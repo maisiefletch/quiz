d6241394d0af02f368afd9c5aa9e06641ebbc5bd
maisiefletch <maisie_fletcher@biswashington.org>
2025-01-23 16:14:38 +0000

    done

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
+score=0
+question ="what is the capital of france?"
+print (question)
+user_answer = input(" your answer")
 
+if user_answer.lower() == "paris":
+    print("correct!")
+    score +=1
+else: 
+    print("Oops! the correct answer is paris.")
+
+question ="what is the capital of UK?"
+print (question)
+user_answer = input(" your answer")
+
+if user_answer.lower() == "london":
+    print("correct!")
+    score +=1
+else: 
+    print("Oops! the correct answer is london.")
+
+
+question ="what is the capital of  netherlands?"
+print (question)
+user_answer = input(" your answer")
+
+if user_answer.lower() == "amsterdam":
+    print("correct!")
+    score +=1
+else: 
+    print("Oops! the correct answer is amsterdam.")
+
+
+question ="what is the capital of sweden?"
+print (question)
+user_answer = input(" your answer")
+
+if user_answer.lower() == "stockholm":
+    print("correct!") 
+    score +=1
+else: 
+    print("Oops! the correct answer is stockholm.")
+
+question ="what is the capital of spain?"
+print (question)
+user_answer = input(" your answer")
+
+if user_answer.lower() == "madrid":
+    print("correct!") 
+    score +=1
+else: 
+    print("Oops! the correct answer is madrid.")
+
+print(f"your score is: {score}")
